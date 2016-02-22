c6809b6fdcecfe48150bdc8b1161ad50bf77ebc0
Pwter <dakcsi@gmail.com>
2016-02-22 11:59:42 +0100

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require ('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.css', function(req,res) {
+  res.sendFile(__dirname + '/index.css');
+});
+
+io.on('connection', function(socket) {
+  io.emit('chat message', 'user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function() {
+    io.emit('chat message', 'user disconnected');
+  });
+});
+
+http.listen(8080, function() {
+  console.log('listening on *:8080');
+});
